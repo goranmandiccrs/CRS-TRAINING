73d771fc0cdf2344168ba935439cb8175028f634
NatasaR <natasar.radovanovic@gmail.com>
2019-03-16 12:30:42 +0100

    created unordered list

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 console.log("Hello World");
-alert("Hello world!");
+//alert("Hello world!");
+
+var array = [];
+
+array[0] = 'Hi Bob';
+array[1] = 'Hi J';
+
+console.log(array);
+
+var newArray = [1, 2, 3, 4, 5, 6];
+
+var listRef = document.querySelector('#myList');
+console.log(listRef);
+//console.log(window);
+
+newArray.forEach(function(el){
+    listRef.innerHTML += "<li>" + el + "</li>"
+});
+
