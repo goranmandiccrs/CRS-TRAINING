dc7bfd227a4423abff97c03a52ae5777ff35d260
Goran Mandic <goran.mandic@crowdedroomstudio.com>
2019-03-16 12:43:06 +0100

    Added onChange callback to input

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ var array = [1, 2, 3, 4, 5, 6];
 var array1 = ["a", "b", "c"];
 
 
-var dummy = function() {
-   console.log("hello");
+var dummy = function(a, b, c) {
+   return a;
+   console.log(a, b, c);
 };
 
+var change = function() {
+  console.log("input changed!!!!");
+};
 
 window.onload = function () {
     var listRef = document.querySelector("#myList");
 
     array1.forEach(function (element, index) {
         listRef.innerHTML += "<li>" + element + "</li>";
-        console.log(index);
     });
-
-
 };
 
 
